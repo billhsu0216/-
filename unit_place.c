584fe7d17542643db9bbd7411f1a6fd6dcb7b27e
billhsu0216 <100885978+billhsu0216@users.noreply.github.com>
2022-05-06 08:42:20 +0800

    Update unit_place.c

--- unit_place.c
+++ unit_place.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+
 int main() {
-    printf("請輸入一個整數:");
-    int i;
-    scanf("%d", &i);
-    //line 12 is incomplete, refer to page 4 of Operation slides to achieve the requirement.
-    printf("這個整數的個位數是%d\n", i);
+	char A[2];
+	while(scanf("%c",&A[0])){
+		if(A[0]=='\n'){
+			printf("%c",A[1]);  //A[0]是enter 所以print A[1]
+			break;	
+		}else{
+			if(A[0]>='0'&&A[0]<='9')
+				A[1]=A[0];  //每次皆把最後一位存在A[1]
+			else{
+				printf("輸入錯誤");  //如果不為數字 跳出
+				break;
+			}	
+		}		
+	}
 }
